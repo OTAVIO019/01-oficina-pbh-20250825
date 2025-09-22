0eb0a25885e44abda78dd34d7be8d92271abb5fb
OTAVIO019 <164563298+OTAVIO019@users.noreply.github.com>
2025-09-22 18:21:30 +0000

    Noticias

--- bd.js
+++ bd.js
@@ -1,4 +1,38 @@
 const usuario = {
-    "email" : "admin@gmail.com",
-    "senha" : "admin"
+    "email": "admin@gmail.com",
+    "senha": "admin"
 };
+
+
+const dados = [
+    {
+        "id": 1,
+        "titulo": "Mobilidade Urbana em BH",
+        "descricao": "Estudo sobre os desafios e soluções para a mobilidade urbana em Belo Horizonte, com foco no transporte público e alternativas sustentáveis.",
+        "conteudo": "Belo Horizonte, como muitas grandes cidades brasileiras, enfrenta sérios desafios quando se trata de mobilidade urbana. O transporte público, em especial, é uma das principais opções para deslocamento da população, mas sofre com superlotação, falta de infraestrutura e problemas relacionados à segurança e conforto dos usuários. Em contrapartida, alternativas como o uso de bicicletas, carros compartilhados e o incentivo ao transporte ativo (como caminhar e pedalar) estão ganhando cada vez mais relevância na cidade. A implementação de novos projetos, como o BRT e ciclovias, é fundamental para melhorar o trânsito e reduzir a emissão de poluentes na cidade.",
+        "categoria": "Transporte e Infraestrutura",
+        "autor": "João Silva",
+        "data": "22/09/2025",
+        "imagem": "assets/imagem/onibus.png"
+    },
+    {
+        "id": 2,
+        "titulo": "Tecnologia 6G",
+        "descricao": "A revolução que a 6G promete trazer para as telecomunicações, com velocidades ultrarrápidas, novas aplicações e transformações sociais.",
+        "conteudo": "O 6G, ou sexta geração de redes móveis, está no horizonte das telecomunicações, prometendo uma transformação ainda mais radical do que o 5G. Espera-se que a 6G seja capaz de atingir velocidades de até 100 vezes mais rápidas do que as atuais redes 5G, com latência quase inexistente e uma capacidade de conectar bilhões de dispositivos simultaneamente. Além das melhorias em velocidade e conectividade, o 6G também deve permitir inovações como realidade aumentada e virtual mais imersivas, carros autônomos, e a integração mais profunda de inteligência artificial nas redes. A expectativa é que as implementações do 6G comecem a surgir em meados da década de 2030, com uma grande transformação digital e social, impactando desde a saúde até a educação e o entretenimento.",
+        "categoria": "Tecnologia e Inovação",
+        "autor": "Ana Costa",
+        "data": "21/09/2025",
+        "imagem": "assets/imagem/Tec6G.png"
+    },
+    {
+        "id": 3,
+        "titulo": "Festival de Música: Knotfest 2025",
+        "descricao": "O Knotfest 2025 promete ser um evento épico, trazendo os maiores nomes do metal, hard rock e outras vertentes pesadas para palcos ao redor do mundo.",
+        "conteudo": "O Knotfest, festival fundado pela banda Slipknot, já se consolidou como um dos maiores eventos de música pesada do mundo. Em 2025, o evento se expande ainda mais, com edições em diversos países e um line-up de peso, com bandas como Slipknot, Metallica, Lamb of God, e novos nomes do cenário alternativo. O festival não se limita apenas à música, oferecendo uma experiência imersiva com exposições, performances artísticas, e até atrações de cultura pop, como estandes de quadrinhos e cosplay. Em 2025, espera-se que o Knotfest continue a unir fãs de música pesada, criando uma atmosfera única de celebração e resistência, com a inconfundível energia de suas apresentações ao vivo.",
+        "categoria": "Música e Cultura",
+        "autor": "Carlos Oliveira",
+        "data": "23/09/2025",
+        "imagem": "assets/imagem/Knotfest.png"
+    }
+]; 
---
+++ login.js
@@ -0,0 +1,19 @@
+const email = usuario.email;
+const senha = usuario.senha;
+
+function verificarCredenciais(){
+    const emailInformado = document.getElementById("email").value; // pega do arquivo index.html
+    const senhaInformada = document.getElementById("senha").value;
+
+    if(emailInformado === email){
+       // alert('E-mail informado corretamente');
+        if(senhaInformada === senha){
+            alert('Senha correta')
+            window.location = "home.html";
+        }else{
+            alert('Senha incorreta')
+        }
+    }else{
+        alert('E-mail incorreto !');
+    }
+}
